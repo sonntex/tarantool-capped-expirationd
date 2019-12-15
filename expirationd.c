63ba99713a490e329cd03e9ce156cedbc9ef3754
sonntex <sonntex@gmail.com>
2019-12-15 18:51:53 +0000

    add transaction

--- expirationd.c
+++ expirationd.c
@@ -3,7 +3,7 @@
 
 static const char nil_key[] = { 0x90 };
 
-static const uint32_t def_scan_tuples_per_it = 1024;
+static const uint32_t def_scan_size = 1024;
 static const uint32_t def_scan_time = 3600;
 
 struct expirationd_task
@@ -15,10 +15,9 @@ struct expirationd_task
   uint32_t space_id;
   uint32_t rm_index_id;
   uint32_t it_index_id;
-  uint32_t it_index_iter;
+  uint32_t it_index_type;
   uint32_t field_no;
-  uint32_t scan_tuples_per_it;
-  uint32_t scan_tuples;
+  uint32_t scan_size;
   uint32_t scan_time;
 };
 
@@ -27,8 +26,9 @@ static struct expirationd_task *task_list = NULL;
 static struct expirationd_task *
 expirationd_alloc()
 {
-  struct expirationd_task *task = (struct expirationd_task *)calloc(1, sizeof(struct expirationd_task));
-  task->scan_tuples_per_it = def_scan_tuples_per_it;
+  struct expirationd_task *task =
+      (struct expirationd_task *)calloc(1, sizeof(struct expirationd_task));
+  task->scan_size = def_scan_size;
   task->scan_time = def_scan_time;
   return task;
 }
@@ -46,14 +46,19 @@ expirationd_find(const char *name)
 }
 
 static bool
+expirationd_breakable(struct expirationd_task *task)
+{
+  return task->it_index_id != task->rm_index_id && task->it_index_type == ITER_GT;
+}
+
+static bool
 expirationd_expired(struct expirationd_task *task, box_tuple_t *tuple)
 {
   const char *buf = box_tuple_field(tuple, task->field_no - 1);
   if (!buf || mp_typeof(*buf) != MP_UINT)
     return false;
-  uint64_t now = fiber_time64() / 1000000;
   uint64_t val = mp_decode_uint(&buf);
-  if (val > now)
+  if (val > fiber_time64() / 1000000)
     return false;
   return true;
 }
@@ -62,57 +67,86 @@ static void
 expirationd_delete(struct expirationd_task *task, box_tuple_t *tuple)
 {
   uint32_t len;
-  const char *str = box_tuple_extract_key(tuple, task->space_id, task->rm_index_id, &len);
+  const char *str = box_tuple_extract_key(
+      tuple,
+      task->space_id,
+      task->rm_index_id,
+      &len);
   box_delete(task->space_id, task->rm_index_id, str, str + len, NULL);
 }
 
-static double
-expirationd_delay(struct expirationd_task *task, uint64_t space_len)
+static bool
+expirationd_iterate(struct expirationd_task *task, box_iterator_t **iterp)
 {
-  double delay = (double)(task->scan_tuples_per_it * task->scan_time) / space_len;
-  if (delay > 1.)
-    delay = 1.;
-  else if (delay < 0.001)
-    delay = 0.001;
-  return delay;
-}
-
-static void
-expirationd_suspend(struct expirationd_task *task)
-{
-  if (++task->scan_tuples >= task->scan_tuples_per_it) {
-    task->scan_tuples = 0;
-    uint64_t space_len = box_index_len(task->space_id, task->it_index_id);
-    if (space_len)
-      fiber_sleep(expirationd_delay(task, space_len));
-  }
-}
-
-static void
-expirationd_iterate(struct expirationd_task *task)
-{
-  box_tuple_t *tuple;
-  box_iterator_t *iter = box_index_iterator(task->space_id, task->it_index_id, task->it_index_iter, nil_key, 0);
-  task->scan_tuples = 0;
-  while (box_iterator_next(iter, &tuple) == 0 && tuple) {
+  box_iterator_t *iter = *iterp;
+  box_txn_begin();
+  for (uint32_t i = 0; i < task->scan_size; ++i) {
+    box_tuple_t *tuple = NULL;
+    if (box_iterator_next(iter, &tuple) < 0) {
+      box_iterator_free(iter);
+      *iterp = NULL;
+      box_txn_rollback();
+      return false;
+    }
+    if (!tuple) {
+      box_iterator_free(iter);
+      *iterp = NULL;
+      box_txn_commit();
+      return true;
+    }
     if (expirationd_expired(task, tuple))
       expirationd_delete(task, tuple);
-    else if (task->it_index_id != task->rm_index_id && task->it_index_iter == ITER_GT)
+    else if (expirationd_breakable(task))
       break;
-    expirationd_suspend(task);
   }
-  box_iterator_free(iter);
+  box_txn_commit();
+  return true;
+}
+
+static bool
+expirationd_suspend(struct expirationd_task *task)
+{
+  double delay = ((double)task->scan_size * task->scan_time) /
+      (box_index_len(task->space_id, task->it_index_id) + 1);
+  if (delay > 1)
+    delay = 1;
+  fiber_set_cancellable(true);
+  fiber_sleep(delay);
+  if (fiber_is_cancelled())
+    return false;
+  fiber_set_cancellable(false);
+  return true;
+}
+
+static void
+expirationd_loop(struct expirationd_task *task)
+{
+  box_iterator_t *iter = NULL;
+  while (true) {
+    if (!iter)
+      iter = box_index_iterator(
+          task->space_id,
+          task->it_index_id,
+          task->it_index_type,
+          nil_key, 0);
+    if (!iter) {
+      say_error("capped-expirationd: index error");
+      break;
+    }
+    if (!expirationd_iterate(task, &iter))
+      break;
+    if (!expirationd_suspend(task))
+      break;
+  }
+  if (iter)
+    box_iterator_free(iter);
 }
 
 static int
 expirationd_work(va_list args)
 {
   struct expirationd_task *task = va_arg(args, struct expirationd_task *);
-  fiber_set_cancellable(1);
-  while (!fiber_is_cancelled()) {
-    expirationd_iterate(task);
-    fiber_sleep(1.f);
-  }
+  expirationd_loop(task);
   return 0;
 }
 
@@ -193,16 +227,16 @@ expirationd_parse_it_index_id(struct expirationd_task *task, const char **pos)
 }
 
 static bool
-expirationd_parse_it_index_iter(struct expirationd_task *task, const char **pos)
+expirationd_parse_it_index_type(struct expirationd_task *task, const char **pos)
 {
   if (mp_typeof(**pos) != MP_STR)
     return false;
   uint32_t len;
   const char *str = mp_decode_str(pos, &len);
   if (strncmp(str, "TREE", len) == 0)
-    task->it_index_iter = ITER_GT;
+    task->it_index_type = ITER_GT;
   else
-    task->it_index_iter = ITER_ALL;
+    task->it_index_type = ITER_ALL;
   return true;
 }
 
@@ -220,7 +254,7 @@ expirationd_parse_it_index(struct expirationd_task *task, const char **pos)
         if (!expirationd_parse_it_index_id(task, pos))
           return false;
       } else if (strncmp(str, "type", len) == 0) {
-        if (!expirationd_parse_it_index_iter(task, pos))
+        if (!expirationd_parse_it_index_type(task, pos))
           return false;
       } else
         mp_next(pos);
@@ -242,11 +276,11 @@ expirationd_parse_field_no(struct expirationd_task *task, const char **pos)
 }
 
 static bool
-expirationd_parse_scan_tuples_per_it(struct expirationd_task *task, const char **pos)
+expirationd_parse_scan_size(struct expirationd_task *task, const char **pos)
 {
   if (mp_typeof(**pos) != MP_UINT)
     return false;
-  task->scan_tuples_per_it = mp_decode_uint(pos);
+  task->scan_size = mp_decode_uint(pos);
   return true;
 }
 
@@ -273,7 +307,7 @@ start(box_function_ctx_t *ctx, const char *args, const char *args_end)
       !expirationd_parse_rm_index(task, &args) ||
       !expirationd_parse_it_index(task, &args) ||
       !expirationd_parse_field_no(task, &args) ||
-      !expirationd_parse_scan_tuples_per_it(task, &args) ||
+      !expirationd_parse_scan_size(task, &args) ||
       !expirationd_parse_scan_time(task, &args)) {
     free(task);
     say_error("capped-expirationd: illegal params");
@@ -286,9 +320,9 @@ start(box_function_ctx_t *ctx, const char *args, const char *args_end)
     return ER_ILLEGAL_PARAMS;
   }
   task->fiber = fiber_new(task->name, &expirationd_work);
-  fiber_set_joinable(task->fiber, 1);
   task->next = task_list;
   task_list = task;
+  fiber_set_joinable(task->fiber, true);
   fiber_start(task->fiber, task);
   return 0;
 }
